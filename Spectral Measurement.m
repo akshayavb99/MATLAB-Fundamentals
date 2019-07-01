c93d1e8c6ba40a1635b7e5db59d6dc08fb5ada7d
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2019-07-01 07:37:36 +0530

    Create Spectral Measurement.m

---
+++ Spectral Measurement.m
@@ -0,0 +1,5 @@
+%Calculating the Speed of a Star Relative to Earth.
+%You can determine the speed of a star relative to the Earth by calculating the amount of redshift
+%It is the change in the wavelengths of light emitted by the star. 
+%One way to determine the amount of redshift is to compare the wavelength of a notable feature with its known value.
+%
